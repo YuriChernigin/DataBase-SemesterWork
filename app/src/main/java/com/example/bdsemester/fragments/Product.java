dddaf397df37154cf7a870e3807bbbfa2dc82056
YuriChernigin <juriichernigin@gmail.com>
2016-05-23 19:13:44 +0300

    (ТОВАРЫ)Реализовано добавление и считвыание данных с БД

--- app/src/main/java/com/example/bdsemester/fragments/Product.java
+++ app/src/main/java/com/example/bdsemester/fragments/Product.java
@@ -1,14 +1,30 @@
 package com.example.bdsemester.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.bdsemester.DataBaseHelper;
 import com.example.bdsemester.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +34,7 @@ import com.example.bdsemester.R;
  * Use the {@link Product#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Product extends Fragment {
+public class Product extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,6 +45,26 @@ public class Product extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    // Добавить запись
+    private Button addRecord;
+    // Ресайклер
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter adapter;
+    // Текст введенный в диалоге
+    private EditText newProductName;
+    // БД
+    private DataBaseHelper dataBaseHelper;
+    private SQLiteDatabase sqLiteDataBase;
+
+    // Временная хуйня. Уже не временная
+    private List <ProductInfo> products;
+    private ProductInfo product;
+    private int idFromDB;
+    private String nameProductFromDB;
+    // Курсор для общения с бд
+    Cursor cursor;
+
 
     /**
      * Use this factory method to create a new instance of
@@ -68,6 +104,45 @@ public class Product extends Fragment {
         View view = inflater.inflate(R.layout.fragment_product, container, false);
         getActivity().setTitle(getString(R.string.product));
 
+        // Инициализация
+        addRecord = (Button) view.findViewById(R.id.btnAddProduct);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewProduct);
+        dataBaseHelper = new DataBaseHelper(getActivity(), DataBaseHelper.DATABASE_NAME, null, 1);
+        sqLiteDataBase = dataBaseHelper.getWritableDatabase();
+        products = new ArrayList<ProductInfo>();
+        product = new ProductInfo();
+        products.clear();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // Курсор для получения данных из таблицы
+        cursor = sqLiteDataBase.query(DataBaseHelper.TABLE_PRODUCT, new String[] {DataBaseHelper.PRODUCT_ID, DataBaseHelper.PRODUCT_NAME},
+                null, null, null, null, null);
+
+
+        // Перемещае курсор на первый элемент и если первый элемент не null, то считываем инфу с бд
+        if(cursor.moveToFirst()) {
+
+
+            // Начиная с первого последовательно считываем все существующие данные с таблицы
+            do {
+                // Добавляем первый элемент в лист с данными о товарах
+                nameProductFromDB = cursor.getString(cursor.getColumnIndex(DataBaseHelper.PRODUCT_NAME));
+                idFromDB = cursor.getInt(cursor.getColumnIndex(DataBaseHelper.PRODUCT_ID));
+                product = new ProductInfo(idFromDB, nameProductFromDB);
+                products.add(product);
+
+            } while (cursor.moveToNext());
+
+            // Передаем в наш адаптер заполненый лист и устнавливаем его в ресайклевью
+            adapter = new RecyclerViewAdapter(products);
+            recyclerView.setAdapter(adapter);
+        }
+
+        // Устанавливаем слушатели
+        addRecord.setOnClickListener(this);
+
         return view;
     }
 
@@ -95,6 +170,49 @@ public class Product extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == addRecord.getId())
+        {
+
+            LayoutInflater inflaterDialog = getActivity().getLayoutInflater();
+            View layout = inflaterDialog.inflate(R.layout.dialog_add_product, null);
+            newProductName = (EditText) layout.findViewById(R.id.editTextProduct);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setView(layout);
+
+
+            builder.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Контент для вставки в таблицу
+                    ContentValues content = new ContentValues();
+                    String nameForInsertDB = newProductName.getText().toString();
+                    content.put(DataBaseHelper.PRODUCT_NAME, nameForInsertDB);
+                    int id = (int)sqLiteDataBase.insert(DataBaseHelper.TABLE_PRODUCT, null, content);
+
+                    products.add(new ProductInfo(id, nameForInsertDB));
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
+            builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast toast = Toast.makeText(getActivity(), "Запись в БД не добавлена", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+
+
+
+            builder.setCancelable(true);
+            builder.create();
+            builder.show();
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -108,6 +226,87 @@ public class Product extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ProductViewHolder>
+    {
+        public List <ProductInfo> products;
+
+        RecyclerViewAdapter(List <ProductInfo> products)
+        {
+            this.products = products;
+        }
+
+        @Override
+        public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_product, parent, false);
+            ProductViewHolder productViewHolder = new ProductViewHolder(view);
+            return productViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ProductViewHolder holder, int position) {
+            holder.productName.setText(products.get(position).getNameProduct());
+            holder.productId.setText(String.valueOf(products.get(position).getIdProduct()));
+        }
+
+        @Override
+        public int getItemCount() {
+            return products.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView)
+        {
+            super .onAttachedToRecyclerView(recyclerView);
+        }
+
+        public class ProductViewHolder extends RecyclerView.ViewHolder
+        {
+            private CardView cardView;
+            private TextView productName;
+            private TextView productId;
+
+            ProductViewHolder (View itemView)
+            {
+                super(itemView);
+                cardView = (CardView) itemView.findViewById(R.id.cardViewProduct);
+                productName = (TextView) itemView.findViewById(R.id.nameProduct);
+                productId = (TextView) itemView.findViewById(R.id.productId);
+
+            }
+        }
+    }
+
+    public class ProductInfo
+    {
+        private int ID;
+        private String nameProduct;
+
+        ProductInfo(){
+            ID = -1;
+            nameProduct = null;
+        };
+
+        ProductInfo(int id, String nameProduct)
+        {
+            this.ID = id;
+            this.nameProduct = nameProduct;
+        }
+
+        public void setData(int id, String nameProduct){
+            this.ID = id;
+            this.nameProduct = nameProduct;
+        }
+
+        public int getIdProduct(){
+            return ID;
+        }
+
+        public String getNameProduct(){
+            return nameProduct;
+        }
+
     }
 
 }
